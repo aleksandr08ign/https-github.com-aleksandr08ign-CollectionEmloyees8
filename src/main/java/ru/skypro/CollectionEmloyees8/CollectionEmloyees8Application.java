f12cd95a648ffd9030639b49eaf7ef2e47f90333
Александр <ignatenko.ap@yandex.ru>
2023-12-06 13:43:52 +0300

    error

---
+++ src/main/java/ru/skypro/CollectionEmloyees8/CollectionEmloyees8Application.java
@@ -0,0 +1,13 @@
+package ru.skypro.CollectionEmloyees8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionEmloyees8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionEmloyees8Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/skypro/CollectionEmloyees8/CollectionEmloyees8ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.CollectionEmloyees8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionEmloyees8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
